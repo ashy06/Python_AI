98efa9aa7cac63454afe6970ed0a62ad698e14e1
ashy06 <91536260+ashy06@users.noreply.github.com>
2023-09-05 21:25:33 +0530

    Create positive_number.py
    Positive numbers from a range

---
+++ positive_number.py
@@ -0,0 +1,16 @@
+a=""
+b=""
+
+list1 = [12, -7, 5, 64, -14]
+for i in list1:
+    if i>0:
+        a+=str(i)+" "
+        
+print(a)
+
+list2 = [12, 14, -95, 3]
+for j in list2:
+    if j>0:
+        b+=str(j)+" "
+        
+print(b)
